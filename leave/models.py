7d97c7fc3102d284166286d507ad1d9da931081f
omambiadauglous <omambiadauglous@gmail.com>
2019-01-08 16:45:03 +0000

    [chore] add more informationon the leave applications

--- leave/models.py
+++ leave/models.py
@@ -19,11 +19,17 @@ class Leave(models.Model):
 class ApplyLeave(models.Model):
     start_date = models.DateField()
     status = models.IntegerField(default = 0)
+
     end_date = models.DateField()
+    resume_date = models.DateField()
+    department = models.CharField(max_length=100)
+    person_taking_charge = models.ForeignKey(Employee, related_name ="person_charge",
+                                        on_delete = models.CASCADE
+                                    ) # From here take his phone number and other details 
     employee = models.ForeignKey(Employee, related_name = "employees",
                                         on_delete = models.CASCADE
                                 )
-
+    home_phone = models.IntegerField(default = 0)
     leave = models.ForeignKey(Leave, related_name = "leave",
                                         on_delete = models.CASCADE
                                 )
