f7f22764a9179a0a1cade55aa9575e130fe7bc15
omambiadauglous <omambiadauglous@gmail.com>
2019-02-14 08:18:28 +0000

    fixed untracked files

--- venv/lib/python3.7/site-packages/django_weasyprint/__init__.py
+++
@@ -1,1 +0,0 @@
-from .views import WeasyTemplateResponseMixin, WeasyTemplateView, WeasyTemplateResponse
--- venv/lib/python3.7/site-packages/easy_pdf/apps.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-
-from django.apps import AppConfig
-
-
-class EasyPDFConfig(AppConfig):
-    name = 'easy_pdf'
