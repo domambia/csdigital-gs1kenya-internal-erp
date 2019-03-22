436b9f75591e263132c8241d45ae4a173189b2a1
omambiadauglous <omambiadauglous@gmail.com>
2019-03-22 08:41:32 +0000

    [chore] add users

---
+++ helpers/utils.py
@@ -0,0 +1,16 @@
+from io import BytesIO # A for stream
+from django.http import HttpResponse
+
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+def render_to_pdf(template_src, context_dir):
+    template = get_template(template_src)
+    html = template.render(context_dir)
+    result = BytesIO()
+
+    pdf = pisa.pisaDocument(BytesIO(html.encode('ISO-8859-1')), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    return None
+
