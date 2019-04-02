dd3ffbc877c1d6d9a78cc08f7a8b302763c30b3a
omambiadauglous <omambiadauglous@gmail.com>
2019-04-02 09:07:21 +0000

    [chore] add website

--- ERP/urls.py
+++ ERP/urls.py
@@ -18,9 +18,10 @@ urlpatterns = [
     url(r'^targets/', include('targets.urls')),
     url(r'^crm/', include('CRM.urls')),
     url(r'^ACCOUNTS/', include('ACCNTS.urls')),
+        url(r'^website/', include('website.urls')),
 ]
 
-if settings.DEBUG: 
-        urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT) 
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
 
 
--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url 
+from django.conf.urls import url
 from . import views
 
-app_name = website 
+
+app_name = "website"
 
 urlpatterns = [
     url(r'^about/', views.about, name = "about"),
+    url(r'^board-of-directos/', views.bod, name = "bod"),
+    url(r'^our-staff/', views.staff, name = "staff"),
 ]
