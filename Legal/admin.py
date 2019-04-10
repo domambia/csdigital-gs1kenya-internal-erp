5317e75be48ee509f89bd04bca355a094879c756
omambiadauglous <omambiadauglous@gmail.com>
2019-04-10 13:50:44 +0000

    [chore] admin sections

--- Legal/admin.py
+++ Legal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, GS1Docs, Contract, Letter
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Letter)
+admin.site.register(GS1Docs)
+admin.site.register(Contract)
