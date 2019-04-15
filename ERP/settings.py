d505694a7c031c36c4baeff3b23955708afd1ce0
omambiadauglous <omambiadauglous@gmail.com>
2019-04-15 07:50:33 +0000

    [fix]

--- ERP/settings.py
+++ ERP/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 #LOGIN_URL = '/users/user_login'
-LOGIN_REDIRECT_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = '/account/login'
 
 BOOTSTRAP4 = {
     'include_jquery': True,
